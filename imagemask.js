4014d69580cdb9b15df4a36b99ee72b07d9248b0
Jacob Buck <buck.jacob@gmail.com>
2015-07-26 14:00:43 +1200

    Complete reweite because I'm bored.

--- imagemask.js
+++ imagemask.js
@@ -1,43 +1,101 @@
 /*!
- * imageMask.js - Copyright (c) 2012 Jacob Buck
- * https://github.com/jacobbuck/imgMask.js
+ * imagemask.js - Copyright (c) 2015 Jacob Buck
+ * https://github.com/jacobbuck/imagemask
  * Licensed under the terms of the MIT license.
  */
-window.imgMask = function ( origimg, mask_url, stretch ) {
-	if ( ! 'HTMLCanvasElement' in window ) 
-		return false;
-	// Some Functions
-	function imagesReady ( images, callback ) {
-		var total  = images.length,
-			loaded = 0;
-		for ( var i = 0; i < total; i++ ) {
-			images[ i ].onload = function(){
-				if ( ++loaded >= total )
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		define(factory);
+	} else {
+		root.imagemask = factory();
+	}
+}(this, function () {
+	// Check if the browser supports html5 canvas
+	var supports = !!window.HTMLCanvasElement;
+
+	// Fire a callback once all images passed have loaded
+	// @param images Array
+	// @param callback Function
+	function imagesReady (images, callback) {
+		var length = images.length,
+		var loaded = 0;
+		for (var i = 0; i < length; i++) {
+			images[i].onload = function () {
+				if (++loaded >= length) {
 					callback();
-			}
+				}
+			};
 		}
 	}
-	var canvas  = document.createElement("canvas"),
-		context = canvas.getContext('2d'),
-		overimg = new Image(),
-		maskimg = new Image(); 
-	overimg.src = origimg.src;
-	maskimg.src = mask_url;
-	origimg.style.visibility = 'hidden';
-	imagesReady( 
-		[ maskimg, overimg ], 
-		function(){
-			canvas.width  = overimg.width;
-			canvas.height = overimg.height;
-			context.globalCompositeOperation = 'source-over';
-			context.drawImage( maskimg, 0, 0, 
-				stretch ? overimg.width : maskimg.width, 
-				stretch ? overimg.height : maskimg.height
-			);
-			context.globalCompositeOperation = 'source-atop';
-			context.drawImage( overimg, 0, 0, overimg.width, overimg.height );
-			origimg.src = canvas.toDataURL('image/png');
-			origimg.style.visibility = 'visible';
+
+	// Check if passed is an image element
+	// @param image Mixed
+	// @return Boolean true if input is image element
+	function isImageNode (image) {
+		return (image && image.nodeType === 1 && image.nodeName === 'IMG');
+	}
+
+	// Fire a callback once all images passed have loaded
+	// @param image Node Image to apply mask to
+	// @param image Node|String Mask image
+	// @param options Object
+	function maskImage (image, mask, options) {
+		options = options || {};
+
+		// Ensure arguments are correct
+		if (!isImageNode(image)) {
+			throw new Error('Expected image argument as <img> element');
 		}
-	);
-};
+		if (!isImageNode(mask) && typeof mask !== 'string') {
+			throw new Error('Expected mask argument as <img> element or string');
+		}
+
+		// Don't bother with older browsers that don't support canvas
+		if (!supports) { return false; }
+
+		// Backup image's origial source into data attribute
+		image.setAttribute('data-original-src', image.src);
+
+		// Create canvas and get context
+		var canvas  = document.createElement('canvas');
+		var context = canvas.getContext('2d');
+
+		// Create new image elements for manipulating image and mask
+		var imageImage = document.createElement('img');
+		var maskImage = document.createElement('img');
+
+		// Once image and mask images have loaded (after source set bellow)
+		imagesReady([imageImage, maskImage], function () {
+
+			// Set canvas to the same size as image
+			canvas.width = imageImage.width;
+			canvas.height = imageImage.height;
+
+			// Draw the mask onto the canvas first
+			context.globalCompositeOperation = 'source-over';
+			context.drawImage(maskImage, 0, 0,
+				options.stretch ? imageImage.width : maskImage.width,
+				options.stretch ? imageImage.height : maskImage.height
+			);
+
+			// Then draw the original image atop of the mask
+			context.globalCompositeOperation = 'source-atop';
+			context.drawImage(imageImage, 0, 0, imageImage.width, imageImage.height);
+
+			// Replace image source with canvas output
+			image.src = canvas.toDataURL('image/png');
+
+			// Fire callback if set
+			if (typeof options.complete === 'function') {
+				options.complete.call(image, image, mask);
+			}
+		});
+
+		// Set the image and mask images source
+		imageImage.src = image.src;
+		maskImage.src = mask.src || mask;
+	}
+
+	// Return maskImage function
+	return maskImage;
+}));
--- jquery.imagemask.js
+++ jquery.imagemask.js
@@ -1,48 +1,20 @@
 /*!
- * imageMask.js - Copyright (c) 2012 Jacob Buck
- * https://github.com/jacobbuck/imgMask.js
+ * jquery.imagemask.js - Copyright (c) 2015 Jacob Buck
+ * https://github.com/jacobbuck/imagemask
  * Licensed under the terms of the MIT license.
  */
-(function($){
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		define(['jquery', 'imagemask'], factory);
+	} else {
+		factory(root.$, root.imagemask);
+	}
+}(this, function ($, imagemask) {
 	$.fn.extend({
-		imgMask: function ( mask_url, stretch ) {
-			// Simple ImagesReady Function
-			function imagesReady ( images, callback ) {
-				var total  = images.length,
-					loaded = 0;
-				$(images).load(function(){
-					if ( ++loaded >= total )
-						callback();
-				});
-			}
-			// Use Canvas Mask
-			if ( 'HTMLCanvasElement' in window )
-				return this.css( 'visibility', 'hidden' ).each(function(){
-					var origimg = this,
-						canvas  = $('<canvas>')[0],
-						context = canvas.getContext('2d'),
-						overimg = new Image(),
-						maskimg = new Image();
-					overimg.src = origimg.src;
-					maskimg.src = mask_url;
-					imagesReady( 
-						[ maskimg, overimg ],
-						function(){
-							canvas.width  = overimg.width;
-							canvas.height = overimg.height;
-							context.globalCompositeOperation = 'source-over';
-							context.drawImage( maskimg, 0, 0, 
-								stretch ? overimg.width : maskimg.width, 
-								stretch ? overimg.height : maskimg.height
-							);
-							context.globalCompositeOperation = 'source-atop';
-							context.drawImage( overimg, 0, 0, overimg.width, overimg.height );
-							$(origimg).attr( 'src', canvas.toDataURL('image/png') ).css( 'visibility','visible' );
-						}
-					);
-				});
-			// Otherwise Do Nothing (only IE 8 and older will get this far)
-			return this;
+		imagemask: function (mask, options) {
+			return this.each(function () {
+				imagemask(this, mask, options);
+			});
 		}
 	});
-}(this.jQuery));
+}));
